061dd148f4908771d97f1ff0153cff4e021e4bda
Brenner Haverlock <80720533+officialbrenner@users.noreply.github.com>
2021-10-04 14:38:45 -0700

    show number of clients connected
    HW #2

--- index.js
+++ index.js
@@ -4,49 +4,35 @@ const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
+let clients = 0;
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
-
-    console.log('Yay, connection was recorded')
-
-    //emit message to all front-end clients
-    io.emit('chat message', 'A user has connected');
-
-    //handling disconnects
-    socket.on('disconnect', function() {
-       io.emit('chat message', 'some user disconnected');
-    });
-
-});
-
-
 
 io.on('connection', (socket) => {
   console.log('a user connected');
-});
+  clients++;
+  socket.broadcast.emit('newClientConnect',{ description: clients + ' clients connected!'});
+  socket.emit('newClientConnect',{ description: clients + ' clients connected!'});
+    // Broadcast when user connects 
 
-io.on('connection', (socket) => {
-    socket.on('chat message', (msg) => {
-      console.log('message: ' + msg);
+  io.emit('chat message', 'A user has connected');
+  socket.on('disconnect', function () {
+        clients--;
+        socket.broadcast.emit('newClientConnect',{ description: clients + ' clients connected!'});
+        io.emit('chat message', 'some user disconnected');
     });
-  });
-
-io.on('connection', (socket) => {
-socket.broadcast.emit('hi');
 });
+
 
 io.on('connection', (socket) => {
     socket.on('chat message', (msg) => {
       io.emit('chat message', msg);
     });
   });
-io.emit('some event', { someProperty: 'some value', otherProperty: 'other value' }); // This will emit the event to all connected sockets
 
-  
 server.listen(3000, () => {
   console.log('listening on *:3000');
 });
